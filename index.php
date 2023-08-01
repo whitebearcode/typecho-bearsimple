1ede80127ba27e570eb46d30184301020a4552be
whitebearcode <whitebear@msn.com>
2023-08-01 22:38:30 +0800

    v2.3.5 release #20230801

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * 简洁清新淡雅，让写作回归内容本身
+ * @package BearSimple 
+ * @author WhiteBear
+ * @version v2.3.5-release
+ * @link https://www.bearnotion.ru/
+ * 
+ */
+
+if (!defined('__TYPECHO_ROOT_DIR__')) exit;
+ $this->need('compoment/head.php');
+
+  ?>
+
+<?php if(Bsoptions('Diy') == true) :?>
+
+<?php $this->need('pages/diy-index.php'); ?>
+
+  <?php endif; ?>
+
+<?php if(Bsoptions('Diy') == false || empty(Bsoptions('Diy'))) :?>
+
+<?php $this->need('pages/index-z.php'); ?>
+
+  <?php endif; ?>
+
+<?php $this->need('compoment/sidebar.php'); ?>
+<?php $this->need('compoment/foot.php'); ?>
---
+++ 配套插件/BsCore/bsRouter.php
@@ -0,0 +1,108 @@
+<?php
+/**
+ * Rules
+ * https://www.bearnotion.ru/
+ */
+namespace TypechoPlugin\BsCore;
+
+require_once 'functions/defines.php';
+require_once 'bsoptions-framework.php';
+use CSF;
+use Utils\Helper;
+
+global $cfs_options;
+$cfs_options = [];
+
+class bsRouter extends \Typecho\Widget
+{
+
+    public static function initRouter()
+    {
+        Helper::addRoute('saveBSoptions', '/bsoptions/save', __CLASS__, 'saveBSoptions');
+        Helper::addRoute('saveBSoptions_ajax', '/bsoptions/ajax', __CLASS__, 'saveBSoptionsByAjax');
+    }
+
+    public static function removeRouter()
+    {
+        Helper::removeRoute('saveBSoptions');
+        Helper::removeRoute('saveBSoptions_ajax');
+    }
+
+    public function saveBSoptions()
+    {
+        $user = \Typecho\Widget::widget('Widget_User');
+        if (!$user->hasLogin()) {
+            echo '用户未登录';
+            die();
+        }
+
+        // Set variables.
+        $plugin = $this->request->get('plugin');
+
+        $obj = get_bs_key_params($plugin);
+        $obj->set_options(true);
+        $this->response->goBack();
+    }
+
+    public function saveBSoptionsByAjax()
+    {
+        $user = \Typecho\Widget::widget('Widget_User');
+        if (!$user->hasLogin()) {
+            $data = [
+                'data' => [
+                    'notice' => '未登录',
+                    'errors' => []
+                ],
+                'success' => false
+            ];
+            $this->response->throwJson($data);
+        }
+
+        $action = $this->request->get('action', null);
+        if (!$action) {
+            $data = [
+                'data' => [
+                    'notice' => '参数错误',
+                    'errors' => []
+                ],
+                'success' => false
+            ];
+            $this->response->throwJson($data);
+        }
+        if (preg_match('/csf_(.*)_ajax_save/i', $action)) {
+            $data = json_decode($_POST['data'], true);
+            $plugin = $data['plugin'];
+
+
+            $obj = get_bs_key_params($plugin);
+            $ret = $obj->set_options(true);
+
+            if ($ret and empty($obj->errors)) {
+                $data = [
+                    'data' => [
+                        'notice' => $obj->notice,
+                        'errors' => $obj->errors
+                    ],
+                    'success' => true
+                ];
+                $this->response->throwJson($data);
+            } else {
+                $data = [
+                    'data' => [
+                        'notice' => $obj->notice,
+                        'errors' => $obj->errors
+                    ],
+                    'success' => true
+                ];
+                $this->response->throwJson($data);
+            }
+
+        } else {
+            $action = str_replace('-', '_', $action);
+            CSF::include_plugin_file('functions/actions.php');
+            $action($this->response);
+        }
+
+    }
+}
+
